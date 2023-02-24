3b471b8ccf84715c1e98ea2c55a7f3b61c9b85a8
Sinan Yilmaz <sinan.yilmaz@assaabloy.com>
2023-02-24 08:54:27 +0000

    String fundamentals

---
+++ javascripts/stringBasics.js
@@ -0,0 +1,14 @@
+let day = "Tuesday";
+console.log(day);
+let subDay = day.slice(0, 4);
+console.log(subDay);
+console.log(day[1]); //u
+
+let splitDay = day.split("s");
+console.log(splitDay[0]);
+console.log(splitDay[1]);
+
+let date = "23";
+let nextDate = "27";
+let diff = parseInt(nextDate) - parseInt(date);
+console.log(diff);
