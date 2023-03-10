b25c0d3a84a0cb360bfbfef4ee4bda367648f305
Sinan Yilmaz <sinan.yilmaz@assaabloy.com>
2023-03-10 14:37:38 +0000

    Tample literals

---
+++ javascripts/templateLiterals.js
@@ -0,0 +1,21 @@
+const message = "This is my\n" + "'first' message";
+
+const another = `This is my 
+'first' message`;
+
+console.log(message);
+//This is my
+//'first' message
+console.log(another);
+//This is my
+//'first' message
+
+const name = "Sinan";
+const anotherTwo = `Hi ${name}
+
+Thanks you for your email
+
+Regards,
+Sinan`;
+
+console.log(anotherTwo);
